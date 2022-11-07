16ad470985d1f39f2c080b361d235fd14889366e
geekyprashant52 <prashant.rohankarnaresh@gmail.com>
2022-11-07 23:18:16 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = require("socket.io")(server, {cors: {
 require("dotenv").config();
 const PORT = process.env.PORT || 8282;
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'https://cognizantcom272-dev-ed.lightning.force.com'}));
 app.use(express());
 app.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
 server.listen(PORT, () => {
