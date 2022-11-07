c7914c78d4387ec262ac8b2ca922129de3b430e8
geekyprashant52 <prashant.rohankarnaresh@gmail.com>
2022-11-07 23:02:10 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const helmet = require("helmet");
+const cors = require("cors");
 
 const INDEX = '/index.html';
 const express = require("express");
@@ -11,6 +12,7 @@ const io = require("socket.io")(server);
 require("dotenv").config();
 const PORT = process.env.PORT || 8282;
 
+app.use(cors());
 app.use(express());
 app.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
 server.listen(PORT, () => {
