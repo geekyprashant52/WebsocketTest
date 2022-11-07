7ef51792475ddd8fc7be9be22b3b6d4cbe40e3df
geekyprashant52 <prashant.rohankarnaresh@gmail.com>
2022-11-07 23:05:06 +0530

    cors

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const express = require("express");
 
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {cors: {
+  origin: '*',
+}});
 require("dotenv").config();
 const PORT = process.env.PORT || 8282;
 
