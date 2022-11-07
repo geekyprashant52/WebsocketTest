be5cff6691804d6186fdd3017349c19d8e3918e7
geekyprashant52 <prashant.rohankarnaresh@gmail.com>
2022-11-07 16:29:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const INDEX = '/index.html';
+const express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+require("dotenv").config();
+const PORT = process.env.PORT || 8282;
+
+app.use(express());
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
+server.listen(PORT, () => {
+  console.log("Listening on port: " + PORT);
+});
+
+
+//EVENT LISTENERS
+
+
+io.on("connection", (socket) => {
+  
+    console.log("User Connected");
+
+    let data = { id: socket.id };
+    socket.emit("set_id", data);
+ 
+    socket.on("disconnect", () => {
+      console.log("User Disconnected");
+    });
+  
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
