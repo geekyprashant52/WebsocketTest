0f4f88360d0ece7866040a4d99f8564d7787b8e3
geekyprashant52 <prashant.rohankarnarnaresh@gmail.com>
2023-03-26 22:15:38 +0530

    fastify added

--- server.js
+++ server.js
@@ -1,69 +1,26 @@
-'use strict';
+const fastify = require('fastify')();
+fastify.register(require('@fastify/websocket'))
 
-const express = require('express');
-const socketIO = require('socket.io');
+fastify.register(async function (fastify) {
+    fastify.get('/fastify', { websocket: true }, (connection /* SocketStream */, req /* FastifyRequest */) => {
+      connection.socket.on('message', message => {
+        // message.toString() === 'hi from client'
+        connection.socket.send('hi from server')
+      })
 
-const PORT = process.env.PORT ;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-
-  /**
-   * Create function to send status
-   * @param success {bool}
-   * @param message {string}
-   */
-  const sendStatus = function({success, message}){
-    socket.emit('status', {success, message});
-  }
-
-  socket.on('transmit', () => {
-    io.emit('chatupdated');
-  });
-
-  socket.on('usertyping', (data) => {
-    io.emit('istyping', data);
-  });
-
-  socket.on('userEnteredChat', () => {
-    io.emit('refreshChatUsers');
-  });
-
-  socket.on('userLeftChat', () => {
-    io.emit('refreshChatUsers');
-  });
-
-  socket.on('usernottyping', (data) => {
-    io.emit('nottyping', data);
-  });
-
-  socket.on('input', (data) => {
-    const name = data.name;
-    const message = data.message;
-
-    if (!name || !message) {
-      sendStatus({
-        success: false,
-        message: 'Please enter a name and message'
-      });
-    } else {
-      socket.emit('output', data);
-      sendStatus({
-        success: true,
-        message: 'Message updating...'
-      });
-    }
+      connection.socket.on('hey', message => {
+        // message.toString() === 'hi from client'
+        connection.socket.send('hey from server')
+      })
+    })
   })
+  
+  
 
-  socket.on('disconnect', () => console.log('Client disconnected'));
-
+fastify.listen({ port: 3000 }, (err, address) => {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at: ${address}`);
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
